392e161c084b3d326dcd05eeef06ce51ab5e71f7
linhcoder2k3 <65477606+linhcoder2k3@users.noreply.github.com>
2021-01-09 22:30:21 +0700

    up

--- api/routes/friend.js
+++ api/routes/friend.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
 const friendController = require('../controllers/friendController');
-router.get('/info', friendController.info);
+router.get('/offer', friendController.offer);
 module.exports = router;
